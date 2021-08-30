ff9a6caa70bc74f82674b867e75edb1f48579acf
Marcos <marcosvto1@gmail.com>
2021-08-30 11:50:47 -0300

    add order placed to capture email in order form placed

--- componenteReactLeads/react/Leads.tsx
+++ componenteReactLeads/react/Leads.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 //import { useCssHandles } from 'vtex.css-handles'
 //import { useQuery } from 'react-apollo'
 import axios from 'axios'
-
+import { Button, Input, Box } from 'vtex.styleguide'
 
 interface LeadsProps {
  
@@ -33,14 +33,20 @@ const Leads: StorefrontFunctionComponent<LeadsProps> = ({}) => {
     setEmail('');
     }
    
-  return (
-    <>
-      <p><input type="text" className="nomeInput" placeholder="nome" value={nome} onChange={e => setNome((e.target as any).value)} /></p>
-      <p><input type="tel" className="telefoneInput" placeholder="telefone" value={telefone} onChange={e => setTelefone((e.target as any).value)} /></p>
-      <p><input type="email" className="emailInput" placeholder="email" value={email} onChange={e => setEmail((e.target as any).value)} /></p>
-      <button type="button" onClick={handlePesquisa} >Cadastrar</button>
-    </>
-  )
+    return (
+      <Box>
+        <p>
+        Agora só inserir seus dados e
+  ficará por dentro de todas nossas
+  promoções!
+        </p>
+        <p><Input type="text" placeholder="Seu nome completo" value={nome} onChange={ (e: any) => setNome((e.target as any).value)} /></p>
+        <p><Input type="tel"  placeholder="telefone" value={telefone} onChange={(e: any) => setTelefone((e.target as any).value)} /></p>
+        <p><Input type="email" placeholder="email" value={email} onChange={(e: any) => setEmail((e.target as any).value)} /></p>
+        <Button type="button" onClick={handlePesquisa} >Cadastrar</Button>
+        
+      </Box>
+    )
 
   }
 export default Leads
