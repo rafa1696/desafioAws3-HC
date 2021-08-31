9a0dc003f62240980165ab74fe4fb9a038400437
Viviane Ribeiro <vivy.saribeiro@gmail.com>
2021-08-30 22:33:21 -0300

    Estilização da leads concluidas, add links imagens da home, fix sliders da home

--- componenteReactLeads/react/Leads.tsx
+++ componenteReactLeads/react/Leads.tsx
@@ -30,18 +30,20 @@ const Leads: StorefrontFunctionComponent<LeadsProps> = ({}) => {
   }
    
     return (
-      <Box>
-        <p>
-        Agora só inserir seus dados e
-  ficará por dentro de todas nossas
-  promoções!
-        </p>
-        <p><Input type="text" placeholder="Seu nome completo" value={nome} onChange={ (e: any) => setNome((e.target as any).value)} /></p>
-        <p><Input type="tel"  placeholder="telefone" value={telefone} onChange={(e: any) => setTelefone((e.target as any).value)} /></p>
-        <p><Input type="email" placeholder="email" value={email} onChange={(e: any) => setEmail((e.target as any).value)} /></p>
-        <Button type="button" onClick={handleCreateLead} >Cadastrar</Button>
+      <div>
+        <h1 style={{textAlign:"center", fontWeight:"bold"} }> Inscreva-se Já!</h1>
+      
+        <Box style={{paddingBottom:16}}>
+          <p style={{textAlign:"center"}}>
+          Agora só inserir seus dados e ficará por dentro de todas nossas promoções!
+          </p>
+          <p style={{textAlign:"center"}}><Input type="text" placeholder="Seu nome completo" value={nome} onChange={ (e: any) => setNome((e.target as any).value)} /></p>
+          <p style={{textAlign:"center"}}><Input type="tel"  placeholder="Seu telefone" value={telefone} onChange={(e: any) => setTelefone((e.target as any).value)} /></p>
+          <p style={{textAlign:"center"}}><Input type="email" placeholder="Seu e-mail" value={email} onChange={(e: any) => setEmail((e.target as any).value)} /></p>
+          <Button type="button" onClick={handleCreateLead} block>Cadastrar</Button>
         
-      </Box>
+        </Box>
+      </div>
     )
 
   }
